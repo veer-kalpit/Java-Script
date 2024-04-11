cfe8a4c3bc0529a54480e10ba5899ad015991c44
Kalpit Thakur <132124428+veer-kalpit@users.noreply.github.com>
2024-04-12 02:53:12 +0530

    practice

---
+++ 01_Variables_data_types/03_Data_type/script.js
@@ -0,0 +1,19 @@
+//  string
+const firstName = 'Kalpit';
+
+// Number 
+const age = 30 ;
+const temp = 98.99;
+
+// Boolean
+
+const hasKid = true;
+const answer = hasKid;
+
+// Null
+const aptNumber = null;
+
+const output = aptNumber;
+
+ 
+
