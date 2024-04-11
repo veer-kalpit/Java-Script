b45ab57063ccdb92f8c58cf7f32f43fa68638d09
Kalpit Thakur <132124428+veer-kalpit@users.noreply.github.com>
2024-04-12 02:08:00 +0530

    practice

---
+++ 01_Variables_data_types/02_variables/script.js
@@ -0,0 +1,14 @@
+let firstName = 'Kalpit' ;
+let lastName = 'Thakur' ;
+console.log(firstName,lastName);
+let age = 22;
+console.log(age);
+const my = {
+   name: 'Kalpit'
+};
+
+my.name = Veer ;
+my.email = veerkalpit@gmail.com;
+
+console.log(my);
+
