da26489d26173ad81be1788fa818d08a6dec17f0
Kalpit Thakur <132124428+veer-kalpit@users.noreply.github.com>
2024-04-19 02:08:25 +0530

    practice

---
+++ 01_Variables_data_types/08_String/script.js
@@ -0,0 +1,29 @@
+let x ;
+
+const name= 'Kalpit';
+const age = 22;
+
+x = 'My name is ' + name + ' and ' + 'i am ' + age + ' years old ' ; 
+
+console.log(x);
+
+// Template literals
+
+let y ; 
+
+const my = 'Kalpit';
+const age1 = 22;
+
+y = ` Call me ${my}  and I am ${age1} years old ` ; 
+
+console.log(y);
+
+// String properties and method
+
+let a;
+
+const s = new String ( 'Hi ') ;
+
+a = typeof s;
+
+console.log(a);
---
+++ 01_Variables_data_types/09_Challenge/script.js
@@ -0,0 +1,14 @@
+const myString = 'kalpit Thakur';
+
+let myNew;
+
+myNew = myString.charAt(0).toUpperCase() + myString.substring(1);
+
+// const name = 'yashit Thakur' ;
+
+// let bigb;
+
+// bigb = name(0).toUpperCase() + name.substring(1);
+
+console.log(myNew);
+// console.log(bigb);
