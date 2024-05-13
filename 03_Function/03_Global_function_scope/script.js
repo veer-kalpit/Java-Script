6912f489d746adafaec6772e5e767832b6a178d7
Kalpit Thakur <132124428+veer-kalpit@users.noreply.github.com>
2024-05-13 12:24:02 +0530

    Practice

---
+++ 03_Function/03_Global_function_scope/script.js
@@ -0,0 +1,11 @@
+const x = 5;
+
+function add()
+{
+   const y= 5;
+   console.log(x+y);
+}
+
+add();
+
+
---
+++ 03_Function/04_Block_scope/script.js
@@ -0,0 +1,39 @@
+const x = 100;
+
+// An If statement is a block
+if (true) {
+  console.log(x);
+  const y = 200;
+  console.log(x + y);
+}
+
+// console.log(y);
+
+// A loop is a block
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+// console.log(i);
+
+// Using var
+if (true) {
+  const a = 500;
+  let b = 600;
+  var c = 700;
+}
+
+console.log(c);
+
+// var IS function scoped
+function run() {
+  var d = 100;
+  console.log(d);
+}
+
+run();
+
+// console.log(d);
+
+const foo = 1;
+var bar = 2;
